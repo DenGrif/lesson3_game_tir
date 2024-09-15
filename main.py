7b71731d577391c214e66723b5f1b1bd7946b20e
DenGrif <grifon991@gmail.com>
2024-09-15 14:06:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("img/apple.png")
 target_width = 50
-target_height = 50
+target_height = 108
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
